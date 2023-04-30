3fca212237d226cbdc083f799f41385b6b30e55d
Articha <tima-1324@mail.ru>
2023-04-30 19:33:27 +0300

    Added settings python file and translations static file

---
+++ src/bot/translations.py
@@ -0,0 +1,8 @@
+from .paths import TRANSLATIONS
+from .settings import Settings
+
+
+class Translations:
+    """Contains translations for bot with different languages"""
+    def __init__(self):
+        pass
