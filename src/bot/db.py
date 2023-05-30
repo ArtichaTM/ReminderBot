74405ba404896f719c494c6ee815911eb4988b64
anna <zinborsh2@gmail.com>
2023-05-30 16:01:50 +0300

    Массивные изменения во внутренней работе класса Reminder

--- src/bot/db.py
+++ src/bot/db.py
@@ -66,24 +66,49 @@ class Database:
         self.db.close()
         self.db = None
 
+
 class Reminder:
-    __slots__ = ('id', '_loaded',
+    __slots__ = ('id', '_id', '_loaded',
                  'datetime', 'owner', 'text',
                  '_datetime', '_owner', '_text')
 
-    def __init__(self, id_: int):
-        self._loaded = None
-        self.id = id_
-        self.datetime = None
-        self.owner = None
-        self.text = None
+    def __init__(
+            self,
+            id_: int,
+            _datetime: datetime = None,
+            _owner: int = None,
+            _text: str = None,
+    
+    ):
+        self._id = self.id = id_
+        self._datetime = self.datetime = _datetime
+        self._owner = self.owner = _owner
+        self._text = self.text = _text
+        self._loaded = False
 
     async def get_by_id(self, id_: int):
         with Settings.Database.cursor() as cur:
             return cur.execute(
                 "SELECT rowid, datetime, owner, text from 'reminds' "
-                f"WHERE rowid == {id_}"
+                f"WHERE rowid=?",
+                (id_,)
             ).fetchone()
+
+    @classmethod
+    async def assert_existing(
+            cls,
+            id: int,
+            owner: int = None,
+            date: datetime = None,
+            text: str = None
+    ) -> 'Reminder':
+        assert isinstance(id, int) or id is None
+        assert isinstance(owner, int) or owner is None
+        assert isinstance(date, datetime) or date is None
+        assert isinstance(text, str) or text is None
+        reminder = cls(id, _owner=owner, _datetime=date, _text=text)
+        reminder._loaded = True
+        return reminder
 
     @classmethod
     async def new(cls) -> 'Reminder':
@@ -93,10 +118,9 @@ class Reminder:
                 "SELECT rowid FROM 'reminds' ORDER BY rowid DESC LIMIT 1;"
             ).fetchone()
             if lastrowid is None:
-                lastrowid = 1
+                return Reminder(0)
             else:
-                lastrowid = lastrowid[0]
-            return Reminder(lastrowid+1)
+                return Reminder(lastrowid[0]+1)
 
     async def get(self, item):
         if item in self.__slots__:
@@ -108,8 +132,10 @@ class Reminder:
             return getattr(self, item)
         raise ValueError(f"There's no field {item} in reminder")
 
-    async def load(self):
-        db: Database = Settings.get('db')
+    async def load(self) -> None:
+        if self._loaded is True:
+            return
+        db: Database = Settings.Database
         if db is None:
             raise RuntimeError('Trying to create reminder class '
                                'before database initialization')
@@ -117,17 +143,24 @@ class Reminder:
             raise RuntimeError("Trying to run load with invalid id")
         reminder = await self.get_by_id(self.id)
         if not reminder:
-            raise ValueError(f"Trying to get reminder with index {self.id} which doesn't exist")
-        _, self.datetime, self.owner, self.text = reminder
+            raise IndexError(f"Trying to get reminder with index {self.id} which doesn't exist")
+        _, self._datetime, self._owner, self._text = reminder
         assert 'timezone' in Settings
-        self.datetime = datetime.fromtimestamp(self.datetime, Settings.timezone)
-        self._owner, self._datetime, self._text = self.owner, self.datetime, self.text
+        self._datetime = datetime.fromtimestamp(self.datetime, Settings.timezone)
+        if self.datetime is None:
+            self.datetime = self._datetime
+        if self.owner is None:
+            self.owner = self._owner
+        if self.text is None:
+            self.text = self._text
         self._loaded = True
 
     async def commit(self) -> None:
 
         # Types
-        if isinstance(self.datetime, datetime):
+        if self.datetime == self._datetime:
+            pass
+        elif isinstance(self.datetime, datetime):
             pass
         elif isinstance(self.datetime, float):
             self.datetime = datetime.fromtimestamp(
@@ -135,20 +168,29 @@ class Reminder:
             )
         else:
             raise ValueError("Datetime should be float or datetime object")
-        if not isinstance(self.owner, int):
-            raise ValueError("Owner ID should be integer")
-        if not isinstance(self.text, str):
-            raise ValueError("Text should be string")
 
-        # Validators
-        if len(self.text) > 2000:
-            raise ValueError("Text of reminder shouldn't be above 2000 characters")
-
-        date_limit = datetime.now(Settings.timezone)+ timedelta(days=366)
+        # Validation
+        date_limit = datetime.now(Settings.timezone) + timedelta(days=366)
         if self.datetime.timestamp() > date_limit.timestamp():
             raise ValueError("Can't set reminder further than on year")
 
+        if self.owner == self._owner:
+            pass
+        elif not isinstance(self.owner, int):
+            raise ValueError("Owner ID should be integer")
+
+        if self.text == self._text:
+            pass
+        elif not isinstance(self.text, str):
+            raise ValueError("Text should be string")
+
+        # Validation
+        if len(self.text) > 2000:
+            raise ValueError("Text of reminder shouldn't be above 2000 characters")
+
         datetime_float = self.datetime.timestamp()
+        if self.id != self._id:
+            raise ValueError("Trying to change reminder id")
         old_reminder = await self.get_by_id(self.id)
         if old_reminder is not None:
             command = (
